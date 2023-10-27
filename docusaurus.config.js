76f954456a6d7b4c61ea8d0e31507aac69163ac4
ruiZ <845880193@qq.com>
2023-10-27 16:46:06 +0800

    change title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Learn CS Site',
+  title: 'CS自学社区',
   tagline: 'TeachYourselfCS',
   favicon: 'img/favicon.ico',
 
