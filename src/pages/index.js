9c5111dadff4007e6ddf155a6913d893817a77f7
ruiZ <845880193@qq.com>
2023-11-02 22:35:54 +0800

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            满怀期待地买一本编程书，无比激动地从网上收藏一套编程教程，这也许就是你学习编程以来最好的感觉。你可能读了几本编程书籍的前几十页，但没有完成任何一本。你可能看了几集编程教程跟着写了几十行代码，但没完成。你在怀疑这样做对吗？这个教程是不是已经过时了？学习这门编程语言有用吗？从一个教程跳到另一个教程，重新学习同样的基本原理。但从未跨越基本原理的学习，因为跨越基本原理需要一些真正的努力。所以你准备好努力学习了嘛✊
           </Link>
         </div>
       </div>
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`自学编程 ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
