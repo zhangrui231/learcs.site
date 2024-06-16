bb72676fe519c84af1e7acdca5b20e69042f149e
ruiZ <845880193@qq.com>
2024-06-16 10:58:26 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,7 +67,11 @@ const config = {
         trackingID: 'c106b2729592653e5e0c26db5e5d3142',
       },
     ],
-    ['./src/plugins/plugin-coze-bot'],
+    ['./src/plugins/plugin-coze-bot',
+      {
+        
+      }
+    ],
   ],
 
   themeConfig:
