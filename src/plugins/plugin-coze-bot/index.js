04559cf120d45d5e5cb695622a4b2dfdb4fec328
ruiZ <845880193@qq.com>
2024-06-17 19:04:00 +0800

    update

--- src/plugins/plugin-coze-bot/index.js
+++ src/plugins/plugin-coze-bot/index.js
@@ -25,7 +25,7 @@ module.exports = function (context, options) {
                     bot_id: '7380921096788590598',
                     },
                     componentProps: {
-                    title: 'AI 指导',
+                    title: 'AI 助教',
                     },
                 });
               });
