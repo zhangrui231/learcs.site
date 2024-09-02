2d0f87fbebd55387a4c72d1c20cd583c90735377
ruiZ <845880193@qq.com>
2024-09-02 10:32:54 +0800

    add baidu verify

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,6 +56,7 @@ const config = {
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
+          metadata: [{ name: 'baidu-site-verification', content: 'codeva-hJyfN2UKgo' }],
         },
       }),
     ],
