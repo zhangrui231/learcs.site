33791fb7d3a8507186c2c547b06ecfe3a25c5777
ruiZ <845880193@qq.com>
2025-03-18 09:52:57 +0800

    add baidu seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,17 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Head from '@docusaurus/Head';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
+    <>
+    <Head>
+    <meta name="baidu-site-verification" content="codeva-hJyfN2UKgo" />
+    </Head>
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
@@ -24,6 +29,7 @@ function HomepageHeader() {
         </div>
       </div>
     </header>
+    </>
   );
 }
 
