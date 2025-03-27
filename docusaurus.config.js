3c7e9459167cc6a8c562c57dd1954623e6a47904
ruiZ <845880193@qq.com>
2025-03-27 19:58:29 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -157,7 +157,7 @@ const config = {
               // },
               {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                href: 'https://discord.gg/WqwvTrEQmA',
               },
               // {
               //   label: 'Twitter',
